f49d658d49146f2f4a8a6e22eda62a90c449a568
Grace Shih <vocalise@gmail.com>
2018-06-05 21:28:35 +0000

    automatically backed up by learn

--- lib/pokemon.rb
+++ lib/pokemon.rb
@@ -1,5 +1,5 @@
 class Pokemon
   def save(pk_name, pk_type, db)
-    db.execute("INSERT INTO pokemon(name, type) VALUES")
+    db.execute("INSERT INTO pokemon(name, type) VALUES(?,?)", pk_name, pk_type);
   end
 end
