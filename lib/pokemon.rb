e1e54053741e392ce61fd8b1adefb89d95f76fc2
Grace Shih <vocalise@gmail.com>
2018-06-05 21:41:49 +0000

    automatically backed up by learn

--- lib/pokemon.rb
+++ lib/pokemon.rb
@@ -12,7 +12,7 @@ class Pokemon
   end
   
   def self.find(pk_id, db)
-    pogo = database_execution.execute("SELECT * FROM pokemon WHERE id = ?;", pk_id)
+    pogo = db.execute("SELECT * FROM pokemon WHERE id = ?;", pk_id)
     pogo
   end
 end
