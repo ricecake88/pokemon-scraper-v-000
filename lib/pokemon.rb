51cd123be20ecef6ef948c0d3599a4980bdc6042
Grace Shih <vocalise@gmail.com>
2018-06-05 22:23:53 +0000

    automatically backed up by learn

--- lib/pokemon.rb
+++ lib/pokemon.rb
@@ -14,7 +14,7 @@ class Pokemon
   
   def self.find(pk_id, db)
     db.execute("SELECT * FROM pokemon WHERE id = ?", pk_id) do |row|
-      pogo = Pokemon.new(row[0], row[1], row[2], db)
+      pogo = Pokemon.new(row)
     end
     pogo
   end
