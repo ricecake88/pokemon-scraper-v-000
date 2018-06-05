bb018a20be331126d95b06b16be10f8417f152af
Grace Shih <vocalise@gmail.com>
2018-06-05 22:00:56 +0000

    automatically backed up by learn

--- lib/pokemon.rb
+++ lib/pokemon.rb
@@ -6,12 +6,12 @@ class Pokemon
   end
   
   def self.save(name, type, db)
-    db.execute("INSERT INTO pokemon(name, type) VALUES(?,?);", name, type)
+    db.execute("INSERT INTO pokemon(name, type) VALUES(?,?)", name, type)
   end
   
   def self.find(pk_id, db)
     binding.pry
-    db.execute("SELECT * FROM pokemon WHERE id = ?;", pk_id) do |row|
+    db.execute("SELECT * FROM pokemon WHERE id = ?", pk_id) do |row|
       Pokemon.new(row[1], row[2], db)
     end
   end
